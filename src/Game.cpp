a4a75e17c9e926994cef3e06b249143f700e96a3
Kai Tamkun <ktamkun@ucsc.edu>
2021-09-01 12:41:52 -0700

    Add cesium

--- src/Game.cpp
+++ src/Game.cpp
@@ -69,6 +69,7 @@ namespace Game2 {
 		add(Resource(this, "Boron").setBasePrice(3.68_e).addTypes("element", "chemical", "metalloid"));
 		add(Resource(this, "Bromine").setBasePrice(4.39_e).addTypes("element", "chemical", "liquid"));
 		add(Resource(this, "Cadmium").setBasePrice(2.73_e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Cesium").setBasePrice(61800._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
