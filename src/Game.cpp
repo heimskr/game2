a8aaa2a11497429b2a6b0cd95d45c102c598f305
Kai Tamkun <ktamkun@ucsc.edu>
2021-09-10 09:29:50 -0700

    Add gadolinium

--- src/Game.cpp
+++ src/Game.cpp
@@ -78,6 +78,7 @@ namespace Game2 {
 		add(Resource(this, "Dysprosium").setBasePrice(307._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Erbium").setBasePrice(26.4_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Europium").setBasePrice(31.4_e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Gadolinium").setBasePrice(28.6_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
