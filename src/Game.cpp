f8f0cbacf64b607584c797cf83b8405ccfed1768
Kai Tamkun <ktamkun@ucsc.edu>
2021-09-09 22:31:41 -0700

    Add europium

--- src/Game.cpp
+++ src/Game.cpp
@@ -77,6 +77,7 @@ namespace Game2 {
 		add(Resource(this, "Cobalt").setBasePrice(32.8_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Dysprosium").setBasePrice(307._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Erbium").setBasePrice(26.4_e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Europium").setBasePrice(31.4_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
