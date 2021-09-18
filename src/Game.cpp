30c4ab36eb890b725447bbfed16339a505e88b5d
Kai Tamkun <ktamkun@ucsc.edu>
2021-09-17 23:14:31 -0700

    Add holmium

--- src/Game.cpp
+++ src/Game.cpp
@@ -84,6 +84,7 @@ namespace Game2 {
 		add(Resource(this, "Gold").setBasePrice(44'800._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Hafnium").setBasePrice(900._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Helium").setBasePrice(24._e).addTypes("element", "chemical", "gas"));
+		add(Resource(this, "Holmium").setBasePrice(57.1_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
