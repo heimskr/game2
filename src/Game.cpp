dfe89deffdf4174ca5331a8353433fd1d5e21db1
Kai Tamkun <ktamkun@ucsc.edu>
2021-11-05 23:00:45 -0700

    Add platinum

--- src/Game.cpp
+++ src/Game.cpp
@@ -102,6 +102,7 @@ namespace Game2 {
 		add(Resource(this, "Osmium").setBasePrice(12000._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Palladium").setBasePrice(49500._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Phosphorus").setBasePrice(2.69_e).addTypes("element", "chemical"));
+		add(Resource(this, "Platinum").setBasePrice(27800._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
