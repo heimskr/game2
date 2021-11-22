63ccec48a2c5e528d3ceca9735510cdbcca7628e
Kai Tamkun <ktamkun@ucsc.edu>
2021-11-21 23:32:37 -0800

    Add rhodium

--- src/Game.cpp
+++ src/Game.cpp
@@ -108,6 +108,7 @@ namespace Game2 {
 		add(Resource(this, "Praseodymium").setBasePrice(103._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Promethium").setBasePrice(460'000._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Rhenium").setBasePrice(3580._e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Rhodium").setBasePrice(147'000._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
