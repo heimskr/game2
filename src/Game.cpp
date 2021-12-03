06f56e7f6badbf143addbb8f3449ec4ac2c0b78d
Kai Tamkun <ktamkun@ucsc.edu>
2021-12-02 22:40:27 -0800

    Add selenium

--- src/Game.cpp
+++ src/Game.cpp
@@ -112,6 +112,7 @@ namespace Game2 {
 		add(Resource(this, "Ruthenium").setBasePrice(10'500._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Samarium").setBasePrice(13.9_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Scandium").setBasePrice(3460._e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Selenium").setBasePrice(21.4_e).addTypes("element", "chemical"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
