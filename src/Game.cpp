384dfa0b9def478830500b6fac12a6d9e928b399
Kai Tamkun <ktamkun@ucsc.edu>
2021-12-06 23:51:21 -0800

    Add sodium

--- src/Game.cpp
+++ src/Game.cpp
@@ -114,6 +114,7 @@ namespace Game2 {
 		add(Resource(this, "Scandium").setBasePrice(3460._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Selenium").setBasePrice(21.4_e).addTypes("element", "chemical"));
 		add(Resource(this, "Silver").setBasePrice(521._e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Sodium").setBasePrice(3._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
