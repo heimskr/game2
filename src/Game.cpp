507effd09bd3f98edb6979371dd111d7c9a6fdb3
Kai Tamkun <ktamkun@ucsc.edu>
2021-12-07 23:17:14 -0800

    Add strontium

--- src/Game.cpp
+++ src/Game.cpp
@@ -115,6 +115,7 @@ namespace Game2 {
 		add(Resource(this, "Selenium").setBasePrice(21.4_e).addTypes("element", "chemical"));
 		add(Resource(this, "Silver").setBasePrice(521._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Sodium").setBasePrice(3._e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Strontium").setBasePrice(6.605_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
