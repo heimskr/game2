b1b049aafc34d9164b770cd2a507a0b58a88908c
Kai Tamkun <ktamkun@ucsc.edu>
2022-01-01 23:43:18 -0800

    Add uranium

--- src/Game.cpp
+++ src/Game.cpp
@@ -130,6 +130,7 @@ namespace Game2 {
 		add(Resource(this, "Tin").setBasePrice(18.7_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Titanium").setBasePrice(11.4_e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Tungsten").setBasePrice(35.3_e).addTypes("element", "chemical", "metal"));
+		add(Resource(this, "Uranium").setBasePrice(101._e).addTypes("element", "chemical", "metal"));
 		add(Resource(this, "Calcium").setBasePrice(2.28_e).addTypes("element", "chemical"));
 		add(Resource(this, "Carbon").setBasePrice(0.122_e).addTypes("element", "chemical"));
 		add(Resource(this, "Copper").setBasePrice(6._e).addTypes("element", "chemical", "metal"));
